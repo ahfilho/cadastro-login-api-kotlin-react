65699e5db5e7f2c3ff7b6d30fa8b5bb26ef5dde2
dinho <dinho-16@hotmail.com>
2022-09-14 13:56:18 -0300

    Save ram: controller, service e entity; OK.

--- src/main/kotlin/com/example/demo/service/RamService.kt
+++ src/main/kotlin/com/example/demo/service/RamService.kt
@@ -1,4 +1,23 @@
 package com.example.demo.service
 
+import com.example.demo.entity.Ram
+import com.example.demo.repository.RamRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+
+@Service
+@Transactional
 class RamService {
+
+    @Autowired
+    lateinit var ramRepository: RamRepository
+
+    fun saveRam(ram: Ram): Ram {
+        return ramRepository.save(ram)
+    }
+    fun List(): MutableList<Ram>{
+        return ramRepository.findAll()
+    }
 }
