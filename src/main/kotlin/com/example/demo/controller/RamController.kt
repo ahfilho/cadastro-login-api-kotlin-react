d23f0f12035bec008b371be3e7139af6276283e8
dinho <dinho-16@hotmail.com>
2022-10-20 00:13:09 -0300

    Ram: controller e service

--- src/main/kotlin/com/example/demo/controller/RamController.kt
+++ src/main/kotlin/com/example/demo/controller/RamController.kt
@@ -1,4 +1,42 @@
 package com.example.demo.controller
 
+import com.example.demo.entity.Ram
+import com.example.demo.service.RamService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/ram")
 class RamController {
+
+@Autowired
+lateinit var ramService: RamService;
+
+    @PostMapping
+    fun save(@RequestBody ram: Ram): Ram? {
+        return ramService.saveRam(ram);
+    }
+    @GetMapping
+    fun list(): List<Ram>{
+        return ramService.listALl();
+    }
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long?,@RequestBody ram: Ram): ResponseEntity<Ram?>?{
+        ram.id
+        return ResponseEntity.ok().body(ramService.update(id!!,ram))
+    }
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long?): HttpStatus?{
+        ramService.delete(id!!)
+        return HttpStatus.OK
+    }
 }
--- src/main/kotlin/com/example/demo/service/RamService.kt
+++ src/main/kotlin/com/example/demo/service/RamService.kt
@@ -1,4 +1,36 @@
 package com.example.demo.service
 
+import com.example.demo.entity.Ram
+import com.example.demo.repository.RamRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+@Service
+@Transactional
 class RamService {
+    @Autowired
+    lateinit var ramRepository: RamRepository;
+
+    fun saveRam(ram: Ram): Ram?{
+        return ramRepository.save(ram)
+    }
+    fun listALl(): MutableList<Ram>{
+        return ramRepository.findAll();
+    }
+    fun update(id:Long, ram:Ram):Ram{
+        val u = ramRepository.findById(id).map {
+            it.brand
+            it.frequency
+            it.size
+        }
+        ramRepository.save(ram)
+        return ram
+    }
+    fun delete(id:Long){
+        val d = ramRepository.findById(id)
+        if(d.isPresent){
+            ramRepository.delete(d.get())
+        }
+    }
 }
