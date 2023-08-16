f89e469e1acc2b4a9ee983e32e88a571a59f6603
arlindo <dinho-16@hotmail.com>
2023-08-16 13:52:15 -0300

    Busca por cpf de cliente. Cadastra apenas se o cpf informado não estiver presente no banco.

--- src/main/kotlin/com/example/demo/repository/ClientRepository.kt
+++ src/main/kotlin/com/example/demo/repository/ClientRepository.kt
@@ -2,6 +2,13 @@ package com.example.demo.repository
 
 import com.example.demo.entity.Client
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
-interface ClientRepository: JpaRepository<Client,Long>{
+interface ClientRepository : JpaRepository<Client, Long> {
+
+
+    @Query("SELECT cl FROM Client cl WHERE cl.cpf = :cpf")
+    fun findByCpf(cpf: String?): Optional<Client>
+
 }
